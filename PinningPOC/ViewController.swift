9002483702781d91b997fa75ac1ac327c6274d3b
Jamie <jamiechu9288@gmail.com>
2020-09-07 21:48:43 -0400

    downloaded and added google certificate into project

--- PinningPOC/ViewController.swift
+++ PinningPOC/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("-=- vdl")
+        URLSession.shared.dataTask(with: .init(url: URL(string: "http://pokeapi.co/api/v2/pokemon/ditto")!)) { (data, respone, error) in
+            print("-=-")
+        }.resume()
     }
 
+    
+    private func getCertificates() -> [SecCertificate] {
+        let url = Bundle.main.url(forResource: "google", withExtension: "cer")
+        return []
+    }
 
 }
 
+//openssl s_client -connect <url>:443 </dev/null | openssl x509 -outform DER -out <filename>.der
